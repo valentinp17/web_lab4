a3348904422b653b2283e2710a6f09e8d5eaea04
Valentin <pvv377@gmail.com>
2019-05-05 21:24:02 +0300

    bd shtyksi

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 
-echo "eeee";
+// db
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connection was successfully established!";
+// /db
 ?>
